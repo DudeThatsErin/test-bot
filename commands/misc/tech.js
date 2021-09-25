a71a7325cc9fa54e10ac8588ca84373476abbc69
Erin Skidds <dudethatserin@outlook.com>
2021-09-25 17:30:00 -0400

    Updated to v13, everything except /help works!

---
+++ commands/misc/tech.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'tech',
+    description: 'Refers user to the Techway Server for additional technical help.',
+    aliases: ['tw', 'techway', 'tech-help', 'th'],
+    usage: '++tech',
+    example: '++tech or ++tw',
+    inHelp: 'yes',
+    userPerms: [''],
+    botPerms: [''],
+    execute(message) {
+
+        message.channel.send('Hey! Not sure if you knew this but you can visit the Techway server for additional help. Here is the invite link: https://discord.gg/cBUetVq');
+    },
+
+};
